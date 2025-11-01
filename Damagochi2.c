e842e745ff77238a6ca51aae794a03080fd16a09
urgamemaker96-ship-it <urgamemaker96@gmail.com>
2025-11-01 13:30:17 +0900

    SwitchSelect 개선
    1. SwitchSelect 네이밍을 유의미한 이름으로 변경
    2. 각 함수에서 받던 num1 매개변수 제거
    3. 메인문 내 분기를 좀 더 깔끔하게 제어

--- Damagochi2.c
+++ Damagochi2.c
@@ -7,7 +7,6 @@
 int AttackAddHealth1(int, int, int);
 int AttackAddHealth2(int, int, int);
 void ShowStatus(int, int, int, int, int, int, int);
-void ShowGuide(); // 바깥라인에다가 함수 선언해줘야 해당 함수 쓸수있다. -> 함수안에 함수 사용할때
 void ShowShop();
 void ShowBattle(int, int, int);
 int Percent(int);
@@ -29,60 +28,210 @@ int gameon = 1; //while 문 안에 또 다른 while 문 만들다 보니 기존 while 문 통제�
 int rewardgold = 8000; // 상점 시스템 만들기 위해서 선언한 변수
 
 
+void SelectSleep();
+
+void SelectPoop();
+
+void SelectEat();
+
+void SelectWalk();
+
+void SelectTug();
+
+void SelectShop();
+
+void SelectGuide();
 
 
-void SwitchSelect1to3(int num1)
+
+
+//함수 정의들 > 함수 리턴 부분과 함수 이름명 앞 맞춰줘야됨 , 그리고 리턴값은 반드시 1개다
+int AttackAddHealth2(int num1, int num2, int num3)
 {
-	switch (num1)
+	int totalDamage = num1 + num2;
+	printf("적이 체력과 공격력을 합친 스킬(총공격력:%d)을 사용했습니다!\n", totalDamage);
+	printf("다마고치에게 %d의 피해를 입혔습니다!\n", totalDamage);
+	return num3 - totalDamage;
+}
+
+int AttackAddHealth1(int num1, int num2, int num3)
+{
+	int totalDamage = num1 + num2;
+	printf("다마고치가 체력과 공격력을 합친 스킬(총공격력:%d)을 사용했습니다!\n", totalDamage);
+	printf("적에게 %d의 피해를 입혔습니다!\n", totalDamage);
+	return num3 - totalDamage;
+
+}
+int Percent(int num1) // 확률조건을 출력해주는 함수
+{
+	return (rand() % 100) < num1; // 0에서99까지 랜덤하게 나오는데 < 70면 0~69까지 반환됨 -> 조건 자체가 반환된다.
+	//0과 1로 판단 -> 조건이 참이면 실행되는거다!
+}
+void ShowStatus(int num1, int num2, int num3, int num4, int num5, int num6, int num7)
+{
+	printf("===================================================\n");
+	printf("상태창\n");
+	printf("체력: %d\n", num1);
+	printf("마나: %d\n", num2);
+	printf("포만감: %d\n", num3);
+	printf("화장실: %d\n", num4);
+	printf("스트레스: %d\n", num5);
+	printf("경험치: %d\n", num6);
+	printf("현재골드: %d\n", num7);
+	printf("===================================================\n");
+}
+void ShowChoice()
+{
+	printf("\n=======================================================================================\n");
+	printf("1.잠자기 2.응가하기 3.밥주기 4.산책하기 5.터그놀이 6.상점 7.다마고치 가이드라인\n");
+	printf("=======================================================================================\n");
+	printf("입력: ");
+
+}
+void ShowBattle(int num1, int num2, int num3)
+{
+	printf("\n----- 전투 -----\n");
+	printf("적 체력: %d\n", num1);
+	printf("내 체력: %d\n", num2);
+	printf("내 마나: %d\n", num3);
+	printf("원하는 숫자를 눌러 대응 하십쇼\n");
+	printf("1.공격 2.스킬 3.도망\n");
+	printf("입력: ");
+}
+void ShowShop()
+{
+	printf("원하는 물품을 구입하여 주십쇼!!\n");
+	// 입력해달라는 안내 메시지를 출력
+	printf("=========================================================================\n");
+	printf("1.최대체력증가포션:200골드 2.공격력증가포션:300골드 3.레벨업포션:400골드\n");
+	printf("=========================================================================\n");
+	printf("입력: ");
+
+}
+
+
+void LevelUpRule()
+{
+	if (exp >= 100 * currentlevel * 1.2)
 	{
-	case 1: //1번을 누르면 잠자기를 시도한다.
-		if (health >= Maxhealth)
-		{
-			printf("더이상 잘 필요가 없습니다.\n");
-			stress += 10;
-		}
-		else
-		{
-			health += 10;
-			printf("다마고치의 체력이 %d 증가했습니다\n", 10);
+		currentlevel++;
+		exp = 0;
+		gochiattack += 10;
+		printf("\n 레벨 업! 현재 레벨: %d\n", currentlevel);
+		printf("공격력이 %d로 증가했습니다!\n", gochiattack);
+	}
 
-		}
+}
 
-		if (mana >= 100)
-		{
-			printf("더이상 마나가 회복될 수 없습니다.\n");
-			stress += 10;
-		}
-		else
-		{
-			mana += 10;
-			printf("다마고치의 마나가 %d 증가했습니다\n", 10);
-		}
-		break;
 
-	case 2: //2번을 누르면 배변활동이 0이되고
-		poo = 0;
-		printf("다마고치의 배변활동지수가 %d 되었습니다\n", poo);
-		break;
+int main(void)
+{
 
-	case 3: //3번을 누르면 포만감이 채워지고 배변지수가 채워진다.
-		if (hungry >= 200)
+	// rand() 초기화는 한 번만!
+	srand((unsigned int)time(NULL)); // 여러번 돌릴 필요없다, 프로그램 실행시 한번만 초기화 해줘도됨
+	while (gameon) // 반복문 지속
+	{
+		//레벨업 조건
+		LevelUpRule();
+		//배변지수가 100을 달성 시 게임오버 상태 -> break를 함수 내에서 처리하지 못할것 같아 남겨둠
+		if (poo >= 100 || stress >= 100)
 		{
-			printf("더이상 먹을 필요가 없습니다.\n");
+			printf("다마고치가 병에 걸렸습니다.\n");
+			printf("게임이 종료되었습니다.\n");
+			break;
 		}
-		else
+		//승리 조건달아주기 -> break를 함수 내에서 처리하지 못할것 같아 남겨둠
+		if (currentlevel == 10)
 		{
-			hungry += 20;
-			poo += 20;
-			printf("다마고치의 포만감이 %d 증가했습니다\n", 20);
+			printf("다마고치가 완전히 성장했습니다!!\n");
+			printf("게임이 종료되었습니다.\n");
+			break;
 		}
-		break;
+		// 다마고치의 체력,포만감,배변활동정도 가 떠야된다
+		ShowStatus(health, mana, hungry, poo, stress, exp, rewardgold);
+		ShowChoice();
+		// 입력해달라는 안내 메시지를 출력
 
+		//1~6번 숫자를 입력할 수 있게 입력을 받는다
+		scanf_s("%d", &num1);
+		switch (num1)
+		{
+		case 1:
+			SelectSleep();
+			break;
+		case 2:
+			SelectPoop();
+			break;
+		case 3:
+			SelectEat();
+			break;
+		case 4:
+			SelectWalk();
+			break;
+		case 5:
+			SelectTug();
+			break;
+		case 6:
+			SelectShop();
+			break;
+		case 7:
+			SelectGuide();
+			break;
+		}
+	}
+
+}
+
+
+void SelectSleep()
+{
+	if (health >= Maxhealth)
+	{
+		printf("더이상 잘 필요가 없습니다.\n");
+		stress += 10;
+	}
+	else
+	{
+		health += 10;
+		printf("다마고치의 체력이 %d 증가했습니다\n", 10);
+
+	}
+
+	if (mana >= 100)
+	{
+		printf("더이상 마나가 회복될 수 없습니다.\n");
+		stress += 10;
+	}
+	else
+	{
+		mana += 10;
+		printf("다마고치의 마나가 %d 증가했습니다\n", 10);
 	}
 }
 
-void SwitchSelect4(int num1)
+void SelectPoop()
 {
+	poo = 0;
+	printf("다마고치의 배변활동지수가 %d 되었습니다\n", poo);
+}
+
+void SelectEat()
+{
+	if (hungry >= 200)
+	{
+		printf("더이상 먹을 필요가 없습니다.\n");
+	}
+	else
+	{
+		hungry += 20;
+		poo += 20;
+		printf("다마고치의 포만감이 %d 증가했습니다\n", 20);
+	}
+}
+
+void SelectWalk()
+{
+	int userInput;
 	if (Percent(50)) //0~49니깐 50퍼센트다
 	{
 		printf("===================================================\n");
@@ -97,9 +246,9 @@ void SwitchSelect4(int num1)
 		while (turnon)
 		{
 			ShowBattle(battle_enemyhealth, health, mana);
-			scanf_s("%d", &num1);
+			scanf_s("%d", &userInput);
 
-			switch (num1) // 분기문 안에 분기문 만들 수 있었다!!!
+			switch (userInput) // 분기문 안에 분기문 만들 수 있었다!!!
 			{
 			case 1: // 공격
 				battle_enemyhealth -= gochiattack;
@@ -191,8 +340,9 @@ void SwitchSelect4(int num1)
 	}
 }
 
-void SwitchSelect5(int num1)
+void SelectTug()
 {
+	int userInput;
 	if (Percent(60))
 	{
 		printf("===================================================\n");
@@ -206,9 +356,9 @@ void SwitchSelect5(int num1)
 		while (turnon)
 		{
 			ShowBattle(battle_enemyhealth, health, mana);
-			scanf_s("%d", &num1);
+			scanf_s("%d", &userInput);
 
-			switch (num1)
+			switch (userInput)
 			{
 			case 1: // 공격
 				battle_enemyhealth -= gochiattack;
@@ -305,194 +455,51 @@ void SwitchSelect5(int num1)
 	}
 }
 
-
-
-
-
-void SwitchSelect6(int num1)
+void SelectShop()
 {
-	
-		if (rewardgold < 200)
+	int userInput;
+
+	if (rewardgold < 200)
+	{
+		printf("돈이 없어 이용할 수 없습니다!!\n");
+
+	}
+	else
+	{
+		ShowShop();
+		//분기문 안에 분기문 만들 수 있다, 입력 받을 변수 num2로 만드려고 했으나 그럴 필요 없어 num1만 씀, 입력받는건 의미가 없다 하나면 충분하다.
+		scanf_s("%d", &userInput);
+		switch (userInput)
 		{
-			printf("돈이 없어 이용할 수 없습니다!!\n");
-			
+		case 1:
+			rewardgold -= 200;
+			Maxhealth += 100;
+
+			printf("최대체력이 %d 증가하여 %d이 되었습니다.", 100, Maxhealth);
+			break;
+
+		case 2:
+			rewardgold -= 300;
+			gochiattack += 50;
+			printf("공격력이 %d 증가하여 %d이 되었습니다.", 50, gochiattack);
+			break;
+
+
+		case 3:
+			rewardgold -= 400;
+			currentlevel += 1;
+			printf("레벨이 %d 증가하여 %d이 되었습니다.", 1, currentlevel);
+			break;
 		}
-		else
-		{
-			ShowShop();
-			//분기문 안에 분기문 만들 수 있다, 입력 받을 변수 num2로 만드려고 했으나 그럴 필요 없어 num1만 씀, 입력받는건 의미가 없다 하나면 충분하다.
-			scanf_s("%d", &num1);
-			switch (num1)
-			{
-			case 1:
-				rewardgold -= 200;
-				Maxhealth += 100;
-
-				printf("최대체력이 %d 증가하여 %d이 되었습니다.", 100, Maxhealth);
-				break;
-
-			case 2:
-				rewardgold -= 300;
-				gochiattack += 50;
-				printf("공격력이 %d 증가하여 %d이 되었습니다.", 50, gochiattack);
-				break;
+	}
 
 
-			case 3:
-				rewardgold -= 400;
-				currentlevel += 1;
-				printf("레벨이 %d 증가하여 %d이 되었습니다.", 1, currentlevel);
-				break;
-			}
-		}
-		
-	
-}
-void SwitchSelect7(int num1)
-{
-
-		ShowGuide();
-	
 }
 
-
-
-
-//함수 정의들 > 함수 리턴 부분과 함수 이름명 앞 맞춰줘야됨 , 그리고 리턴값은 반드시 1개다
-int AttackAddHealth2(int num1, int num2, int num3)
-{
-	int totalDamage = num1 + num2;
-	printf("적이 체력과 공격력을 합친 스킬(총공격력:%d)을 사용했습니다!\n", totalDamage);
-	printf("다마고치에게 %d의 피해를 입혔습니다!\n", totalDamage);
-	return num3 - totalDamage;
-}
-
-int AttackAddHealth1(int num1, int num2, int num3)
-{
-	int totalDamage = num1 + num2;
-	printf("다마고치가 체력과 공격력을 합친 스킬(총공격력:%d)을 사용했습니다!\n", totalDamage);
-	printf("적에게 %d의 피해를 입혔습니다!\n", totalDamage);
-	return num3 - totalDamage;
-
-}
-int Percent(int num1) // 확률조건을 출력해주는 함수
-{
-	return (rand() % 100) < num1; // 0에서99까지 랜덤하게 나오는데 < 70면 0~69까지 반환됨 -> 조건 자체가 반환된다.
-	//0과 1로 판단 -> 조건이 참이면 실행되는거다!
-}
-void ShowStatus(int num1, int num2, int num3, int num4, int num5, int num6, int num7)
-{
-	printf("===================================================\n");
-	printf("상태창\n");
-	printf("체력: %d\n", num1);
-	printf("마나: %d\n", num2);
-	printf("포만감: %d\n", num3);
-	printf("화장실: %d\n", num4);
-	printf("스트레스: %d\n", num5);
-	printf("경험치: %d\n", num6);
-	printf("현재골드: %d\n", num7);
-	printf("===================================================\n");
-}
-void ShowChoice()
-{
-	printf("\n=======================================================================================\n");
-	printf("1.잠자기 2.응가하기 3.밥주기 4.산책하기 5.터그놀이 6.상점 7.다마고치 가이드라인\n");
-	printf("=======================================================================================\n");
-	printf("입력: ");
-
-}
-void ShowBattle(int num1, int num2, int num3)
-{
-	printf("\n----- 전투 -----\n");
-	printf("적 체력: %d\n", num1);
-	printf("내 체력: %d\n", num2);
-	printf("내 마나: %d\n", num3);
-	printf("원하는 숫자를 눌러 대응 하십쇼\n");
-	printf("1.공격 2.스킬 3.도망\n");
-	printf("입력: ");
-}
-void ShowShop()
-{
-	printf("원하는 물품을 구입하여 주십쇼!!\n");
-	// 입력해달라는 안내 메시지를 출력
-	printf("=========================================================================\n");
-	printf("1.최대체력증가포션:200골드 2.공격력증가포션:300골드 3.레벨업포션:400골드\n");
-	printf("=========================================================================\n");
-	printf("입력: ");
-
-}
-void ShowGuide()
+void SelectGuide()
 {
 	printf("다마고치는 스트레스가 100을 찍거나 배변지수가 100일 경우 사망 그리고 전투 시 체력이 떨어지면 사망합니다.\n");
 	printf("다마고치는 레벨이 10이되면 최종성장하며 게임이 클리어 됩니다..\n");
 	printf("다마고치는 불가능한 선택을 할때 ,도망칠때 스트레스를 10씩 받습니다.\n");
 	printf("다마고치는 잠을 자거나, 적을 쓰러뜨릴 경우 스트레스를 회복합니다 .\n");
 }
-
-void LevelUpRule() 
-{
-	if (exp >= 100 * currentlevel * 1.2)
-	{
-		currentlevel++;
-		exp = 0;
-		gochiattack += 10;
-		printf("\n 레벨 업! 현재 레벨: %d\n", currentlevel);
-		printf("공격력이 %d로 증가했습니다!\n", gochiattack);
-	}
-
-}
-
-
-int main(void)
-{
-
-	// rand() 초기화는 한 번만!
-	srand((unsigned int)time(NULL)); // 여러번 돌릴 필요없다, 프로그램 실행시 한번만 초기화 해줘도됨
-	while (gameon) // 반복문 지속
-	{
-		//레벨업 조건
-		LevelUpRule();
-		//배변지수가 100을 달성 시 게임오버 상태 -> break를 함수 내에서 처리하지 못할것 같아 남겨둠
-		if (poo >= 100 || stress >= 100)
-		{
-			printf("다마고치가 병에 걸렸습니다.\n");
-			printf("게임이 종료되었습니다.\n");
-			break;
-		}
-		//승리 조건달아주기 -> break를 함수 내에서 처리하지 못할것 같아 남겨둠
-		if (currentlevel == 10) 
-		{
-			printf("다마고치가 완전히 성장했습니다!!\n");
-			printf("게임이 종료되었습니다.\n");
-			break;
-		}
-		// 다마고치의 체력,포만감,배변활동정도 가 떠야된다
-		ShowStatus(health, mana, hungry, poo, stress, exp, rewardgold);
-		ShowChoice();
-		// 입력해달라는 안내 메시지를 출력
-
-		//1~6번 숫자를 입력할 수 있게 입력을 받는다
-		scanf_s("%d", &num1);
-		SwitchSelect1to3(num1); // 1~3은 간단하니깐 switch 분기문으로 함수를 만들어 줬다.
-		switch (num1)
-		{
-		case 4: // 산책하기
-			SwitchSelect4(num1);
-			break;
-		case 5: // 터그놀이
-			SwitchSelect5(num1);
-			break;
-		case 6:
-			SwitchSelect6(num1); // 그냥 써주면 입력만 먹히고 나머지 기능안먹힘 -> case 6 와 break로 제어해줘야됨
-			break;
-		case 7:
-			SwitchSelect7(num1); // 그냥 써주면 입력만 먹히고 나머지 기능안먹힘 -> case 6 와 break로 제어해줘야됨
-			break;
-		}
-	}
-
-}
-
-
-
-
